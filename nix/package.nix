4c7eddf0e8a8575cb02843edf04f6b327d00350a
vdbe <vdbewout@gmail.com>
2025-05-29 16:25:18 +0200

    chore: remove rec

--- nix/package.nix
+++ nix/package.nix
@@ -13,71 +13,77 @@
 let
   fs = lib.fileset;
 in
-rustPlatform.buildRustPackage rec {
-  pname = passthru.cargoTOML.package.name;
-  inherit (passthru.cargoTOML.package) version;
+rustPlatform.buildRustPackage (
+  final:
+  let
+    cargoTOML = lib.importTOML "${final.src}/Cargo.toml";
+  in
+  {
+    pname = cargoTOML.package.name;
+    inherit (cargoTOML.package) version;
 
-  src = fs.toSource {
-    root = ../.;
-    fileset = fs.intersection (fs.gitTracked ../.) (
-      fs.unions [
-        ../Cargo.lock
-        ../Cargo.toml
-        ../src
+    src = fs.toSource {
+      root = ../.;
+      fileset = fs.intersection (fs.gitTracked ../.) (
+        fs.unions [
+          ../Cargo.lock
+          ../Cargo.toml
+          ../src
 
-        ../rustfmt.toml
-      ]
-    );
-  };
-
-  cargoLock.lockFile = ../Cargo.lock;
-
-  nativeBuildInputs =
-    (lib.optional stdenv.hostPlatform.isStatic removeReferencesTo)
-    ++ (lib.optional optimizeWithUpx upx);
-
-  doInstallCheck = true;
-  nativeInstallCheckInputs = [ versionCheckHook ];
-  versionCheckProgramArg = [ "--version" ];
-
-  # `-C panic="abort"` breaks checks
-  doCheck = !optimizeSize;
-
-  postFixup = toString [
-    (lib.optionalString stdenv.hostPlatform.isStatic ''
-      find "$out" \
-        -type f \
-        -exec remove-references-to -t ${stdenv.cc.libc} '{}' +
-    '')
-    (lib.optionalString optimizeWithUpx ''
-      upx --best --lzma "$out/bin/sshd-command"
-    '')
-  ];
-
-  env =
-    let
-      rustFlags =
-        lib.optionalAttrs lto {
-          lto = "fat";
-          embed-bitcode = "yes";
-        }
-        // lib.optionalAttrs optimizeSize {
-          codegen-units = 1;
-          opt-level = "s";
-          panic = "abort";
-          strip = "symbols";
-        };
-    in
-    {
-      RUSTFLAGS = toString (lib.mapAttrsToList (name: value: "-C ${name}=${toString value}") rustFlags);
+          ../rustfmt.toml
+        ]
+      );
     };
 
-  passthru = {
-    cargoTOML = lib.importTOML ../Cargo.toml;
-  };
+    cargoLock.lockFile = ../Cargo.lock;
 
-  meta = {
-    license = lib.licenses.mit;
-    mainProgram = "sshd-command";
-  };
-}
+    nativeBuildInputs =
+      (lib.optional stdenv.hostPlatform.isStatic removeReferencesTo)
+      ++ (lib.optional optimizeWithUpx upx);
+
+    doInstallCheck = true;
+    nativeInstallCheckInputs = [ versionCheckHook ];
+    versionCheckProgramArg = [ "--version" ];
+
+    # `-C panic="abort"` breaks checks
+    doCheck = !optimizeSize;
+
+    postFixup = toString [
+      (lib.optionalString stdenv.hostPlatform.isStatic ''
+        find "$out" \
+          -type f \
+          -exec remove-references-to -t ${stdenv.cc.libc} '{}' +
+      '')
+      (lib.optionalString optimizeWithUpx ''
+        upx --best --lzma "$out/bin/sshd-command"
+      '')
+    ];
+
+    env =
+      let
+        rustFlags =
+          lib.optionalAttrs lto {
+            lto = "fat";
+            embed-bitcode = "yes";
+          }
+          // lib.optionalAttrs optimizeSize {
+            codegen-units = 1;
+            opt-level = "s";
+            panic = "abort";
+            strip = "symbols";
+          };
+      in
+      {
+        RUSTFLAGS = toString (lib.mapAttrsToList (name: value: "-C ${name}=${toString value}") rustFlags);
+      };
+
+    passthru = {
+      inherit cargoTOML;
+    };
+
+    meta = {
+      license = lib.licenses.mit;
+      mainProgram = "sshd-command";
+    };
+  }
+)
